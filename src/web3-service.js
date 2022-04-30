86140ab144e7d4b54dbf3ec165bb2d8106d1bb6a
Antony Chang <54241458+AntonyChang@users.noreply.github.com>
2022-04-30 11:16:53 +0200

    Add files via upload

--- src/web3-service.js
+++ src/web3-service.js
@@ -1,149 +1,106 @@
-import { ERC20ABI, MultisenderABI } from './abis'
-import { sum } from './utils'
+import { ERC20ABI, MultisenderABI } from "./abis";
+import { sum } from "./utils";
+
+/*
+ * -------------- Updated SendToken Method ------------------
+ */
 
 export async function sendToken(tokenAddress, userAddress, sendData) {
-    const multiSenderAddress = process.env.REACT_APP_MULTISENDER_ADDRESS;
-    
-    const addresses = sendData.map(d => d.address);
-    const amounts = sendData.map(d => d.amount);
+  // Get Multisender Contract Address
+  const multiSenderAddress = "0x186D606d727158349810B4f7859FBD2e652EEAAD";
 
-    try {
+  //   Extract Addresses and Amounts in separate arrays
+  const addresses = sendData.map((d) => d.address);
+  const amounts = sendData.map((d) => d.amount);
 
-        const web3 = window.web3;
+  try {
+    const web3 = window.web3;
+    // Get Token Contract
 
-        // 1. Transfer total amounts for Multisender contract to send
-        const contract = new web3.eth.Contract(ERC20ABI, tokenAddress);
-        const userTokenBalance = await contract.methods.balanceOf(userAddress).call();
-        const decimal = await contract.methods.decimals().call();
-        const tokenDecimals = web3.utils.toBN(decimal); 
+    const contract = new web3.eth.Contract(ERC20ABI, tokenAddress);
 
-        const totalAmount = amounts.reduce((a, c) => a + c); 
-        const totalAmountToSend = web3.utils.toHex(web3.utils.toBN(totalAmount).mul(web3.utils.toBN(10).pow(tokenDecimals)));
-        
-        if (userTokenBalance / Math.pow(10, decimal) < totalAmount) throw new Error('Insufficient token balance'); 
-        
-        const gasPrice = await web3.eth.getGasPrice();
+    // Get Token Data
+    const userTokenBalance = await contract.methods
+      .balanceOf(userAddress)
+      .call();
+    const decimal = await contract.methods.decimals().call();
+    const tokenDecimals = web3.utils.toBN(decimal);
 
-        await contract.methods.approve(multiSenderAddress, totalAmountToSend).send({
-            from: userAddress,
-            //gas: gas,
-            gasPrice: gasPrice,
-        });
+    const totalAmount = amounts.reduce((a, c) => a + c);
+    const totalAmountToSend = web3.utils.toHex(
+      web3.utils.toBN(totalAmount).mul(web3.utils.toBN(10).pow(tokenDecimals))
+    );
 
-        // await contract.methods.transfer(multiSenderAddress, totalAmountToSend).send({
-        //     from: userAddress,
-        //     //gas: gas,
-        //     gasPrice: gasPrice,
-        // });
-        console.log('>>> gasPrice', await web3.eth.getGasPrice());
-        
-        // Get gas amount for single transaction of max amount
-        const maxAmount = Math.max(...amounts);
-        const maxAmountToSend = web3.utils.toHex(web3.utils.toBN(maxAmount).mul(web3.utils.toBN(10).pow(tokenDecimals)));
+    if (userTokenBalance / Math.pow(10, decimal) < totalAmount)
+      throw new Error("Insufficient token balance");
 
-        const gasForSingleTransaction = await contract.methods.transfer(addresses[0], maxAmountToSend).estimateGas({
-            from: userAddress
-        }); console.log('>>>>> gasForSingleTransaction', gasForSingleTransaction)
+    const gasPrice = await web3.eth.getGasPrice();
 
-        // 2. Send amounts to receivers from Multisender contract
-        const multisender = new window.web3.eth.Contract(MultisenderABI, multiSenderAddress);
-        const amountsToSend = amounts.map(a => web3.utils.toHex(web3.utils.toBN(a).mul(web3.utils.toBN(10).pow(tokenDecimals))));
+    // Transfer total amount to multisender contract
+    await contract.methods
+      .transfer(multiSenderAddress, totalAmountToSend)
+      .send({
+        from: userAddress,
+        gasPrice: gasPrice,
+      });
 
-        const pageSize = 500;
-        const txArray = [];
-        for (let i = 0; i <= parseInt(addresses.length / pageSize); i++) {
-            const pageAddresses = addresses.slice(i * pageSize, (i + 1) * pageSize);
-            const pageAmountsToSend = amountsToSend.slice(i * pageSize, (i + 1) * pageSize);
+    // Get gas amount for single transaction of max amount
+    const maxAmount = Math.max(...amounts);
+    const maxAmountToSend = web3.utils.toHex(
+      web3.utils.toBN(maxAmount).mul(web3.utils.toBN(10).pow(tokenDecimals))
+    );
 
-            const gas = await multisender.methods.bulkSendTokens(tokenAddress, pageAddresses, pageAmountsToSend).estimateGas({
-                from: userAddress
-            });
-            console.log('>>>>>> Gas Amount', gas);
+    // ---------------------------------------------------------------------------------
+    // We are not transferring here, this is just gas estimation of maximum amount transfer
+    const gasForSingleTransaction = await contract.methods
+      .transfer(addresses[0], maxAmountToSend)
+      .estimateGas({
+        from: userAddress,
+      });
+    console.log(">>>>> gasForSingleTransaction", gasForSingleTransaction);
+    //  ---------------------------------------------------------------------------------
 
-            const sendData = {
-                from: userAddress,
-                // gas: gas,
-                gasPrice: gasPrice,
-            };
+    // 2. Send amounts to receivers from Multisender contract
 
-            // Set contract owner fee as payable amount
-            if (gas < gasForSingleTransaction * pageAddresses.length) sendData.value = web3.utils.toWei(`${gasForSingleTransaction * pageAddresses.length - gas}`, 'gwei')
-            
-            txArray.push(
-                await multisender.methods.bulkSendTokens(tokenAddress, pageAddresses, pageAmountsToSend).send(sendData)
-            );
-        }
-        return txArray;
-    } catch (e) {
-        console.error(e);
-        throw e;
-    }
+    // Get MultiSender Contract
+    const multisender = new window.web3.eth.Contract(
+      MultisenderABI,
+      multiSenderAddress
+    );
+
+    // Call bulkSendTokens
+    const amountsToSend = amounts.map((a) =>
+      web3.utils.toHex(
+        web3.utils.toBN(a).mul(web3.utils.toBN(10).pow(tokenDecimals))
+      )
+    );
+
+    //   Gas Estimation
+    const gas = await multisender.methods
+      .bulkSendTokens(tokenAddress, addresses, amountsToSend)
+      .estimateGas({
+        from: userAddress,
+      });
+    console.log(">>>>>> Gas Amount", gas);
+
+    const sendData = {
+      from: userAddress,
+      gasPrice: gasPrice,
+    };
+
+    const txArray = [];
+
+    txArray.push(
+      await multisender.methods
+        .bulkSendTokens(tokenAddress, addresses, amountsToSend)
+        .send(sendData)
+    );
+
+    return txArray;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-export async function sendBNB(sendData) {
-    const userAddress = window.ethereum.selectedAddress;
-    
-    const multiSenderAddress = process.env.REACT_APP_MULTISENDER_ADDRESS;
-    
-    const addresses = sendData.map(d => d.address);
-    const amounts = sendData.map(d => d.amount);
-
-    try {
-
-        const web3 = window.web3;
-
-        // 1. Transfer total amounts for Multisender contract to send
-        const decimal = 18;
-        const userBalance = await window.web3.eth.getBalance(userAddress);  console.log('>>>>> userBalance', userBalance);
-
-        const totalAmount = sum(amounts); 
-        
-        if (userBalance < totalAmount * (10 ** decimal)) throw new Error('Insufficient balance'); 
-        
-        const gasPrice = await web3.eth.getGasPrice();
-        
-        // Get gas amount for single transaction of max amount
-        const maxAmount = Math.max(...amounts);
-        const maxAmountToSend = web3.utils.toHex(web3.utils.toBN(maxAmount * 10 ** decimal));
-
-        const gasForSingleTransaction = await web3.eth.estimateGas({from: userAddress, to: addresses[0], value:maxAmountToSend}); console.log('>>>>> gasForSingleTransaction', gasForSingleTransaction)
-
-        // 2. Send amounts to receivers from Multisender contract
-        const multisender = new window.web3.eth.Contract(MultisenderABI, multiSenderAddress);
-        const amountsToSend = amounts.map(a => web3.utils.toBN(a * 10 ** decimal));
-
-        const pageSize = 500;
-        const txArray = [];
-        for (let i = 0; i <= parseInt(addresses.length / pageSize); i++) {
-            const pageAddresses = addresses.slice(i * pageSize, (i + 1) * pageSize);
-            const pageAmountsToSend = amountsToSend.slice(i * pageSize, (i + 1) * pageSize);
-            let value = pageAmountsToSend.reduce((a, c) => a.add(c)); console.log('=============', web3.utils.toHex(value))
-
-            const gas = await multisender.methods.bulkSendBNB(pageAddresses, pageAmountsToSend).estimateGas({
-                from: userAddress,
-                value: web3.utils.toHex(value)
-            });
-            console.log('>>>>>> Gas Amount', gas);
-
-            const sendData = {
-                from: userAddress,
-                // gas: gas,
-                gasPrice: gasPrice
-            };
-
-            // Set contract owner fee as payable amount
-            if (gas < gasForSingleTransaction * pageAddresses.length) 
-                value = value.add(web3.utils.toBN(gasForSingleTransaction * pageAddresses.length - gas))
-            
-            sendData.value = web3.utils.toHex(value);
-            console.log('>>>>>>>>>>>>> senddata', sendData);
-            txArray.push(
-                await multisender.methods.bulkSendBNB(pageAddresses, pageAmountsToSend).send(sendData)
-            );
-        }
-        return txArray;
-    } catch (e) {
-        console.error(e);
-        throw e;
-    }
-}
+// Empty Method
+export function sendBNB() {}
